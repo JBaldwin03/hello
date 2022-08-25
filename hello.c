518756a874c8401f196612a13d7080b961063f15
JBaldwin03 <jbaldwin11@huskers.unl.edu>
2022-08-25 17:42:15 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Jack Baldwin
+ * Date: 2022/25/08
+ *
+ * A simple Hello world program that says my name
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+    printf("Jack Baldwin\n");
+
+    return 0;
+ }
